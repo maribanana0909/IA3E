5fd39751b217175e199eba8436214e5659a00cc2
maribanana0909 <146956453+maribanana0909@users.noreply.github.com>
2024-08-02 09:27:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPrincipal = document.querySelector('.caixa-pergunta');
+const caixaPrincipal = document.querySelector('.caixa-alternativa');
+const caixaPrincipal = document.querySelector('.caixa-resultado');
+const caixaPrincipal = document.querySelector('.texto-resultado');
+
+const perguntas [//abre a lista de objetos (itens)
+{//abre o item
+    enunciado:"pergunta 1",
+    alternativas:[
+        "alternativa 1"
+        "alternativa 2"
+    ]
+}
+]
