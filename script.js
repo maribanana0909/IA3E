ed7c64a900e1945dbefe745724ba9f789f98d87a
maribanana0909 <146956453+maribanana0909@users.noreply.github.com>
2024-08-08 07:44:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const caixaPrincipal = document.querySelector('.caixa-principal');
-const caixaPrincipal = document.querySelector('.caixa-pergunta');
-const caixaPrincipal = document.querySelector('.caixa-alternativa');
-const caixaPrincipal = document.querySelector('.caixa-resultado');
-const caixaPrincipal = document.querySelector('.texto-resultado');
+const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
 
-const perguntas [//abre a lista de objetos (itens)
-{//abre o item
-    enunciado:"pergunta 1",
-    alternativas:[
-        "alternativa 1"
-        "alternativa 2"
-    ]
+const perguntas = [//abre a lista de objetos (itens)
+    {//abre o item
+        enunciado:"Perguntas 1",
+        alternativas: ["Alternativa 1", "Alternativa 2"],
+ },
+{
+    enunciado: "Perguntas 2",
+        alternativas: ["Alternativa 1", "Alternativa 2"]
+},
+{
+    enunciado: "Perguntas 3",
+        alternativas: ["Alternativa 1", "Alternativa 2"]
 }
 ]
+let posicao = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+
+}
