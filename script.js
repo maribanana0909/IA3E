8c87f0fcec6aeab34e95422dfa15e544f483de95
maribanana0909 <146956453+maribanana0909@users.noreply.github.com>
2024-10-11 09:15:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,43 +2,45 @@ const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPergunta = document.querySelector('.caixa-pergunta');
 const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
 
 const perguntas = [//abre a lista de objetos (itens)
     {//abre o item
-        enunciado:"você gosta de ler livros?",
-        alternativas:[{
-            texto:"sim",
-            afirmação:"porque é meu momento de lazer"
-        },
-        {
-            texto:"não",
-            afirmação: "porque não tenho paciência"
-        }
-        ]   
-    },
-    {
-        enunciado:"você gosta de ir ao cinema?",
-        alternativas:[{
-            texto:"sim",
-            afirmação: "porque é meu momento de lazer"
+        enunciado: "você gosta de ler livros?",
+        alternativas: [{
+            texto: "sim",
+            afirmação: "você gosta de ler livros? sim, porque é meu momento de lazer."
         },
         {
             texto: "não",
-            afirmação: "porque não gosto de sair de casa"
+            afirmação: "você gosta de ler livros? não, porque não tenho paciência."
         }
         ]
     },
     {
-        enunciado:"você ouve música regularmente?",
-        alternativas:[{
-            texto:"sim",
-             afirmação: "porque me espairece"
+        enunciado: "você gosta de ir ao cinema?",
+        alternativas: [{
+            texto: "sim",
+            afirmação: "você gosta de ir ao cinema? sim, porque gosto do ambiente e da pipoca."
+
+        },
+        {
+            texto: "não",
+            afirmação: "você gosta de ir ao cinema? não, porque não gosto de sair de casa."
+        }
+        ]
     },
-    { 
-           texto:"não",
-           afirmação: "porque so me irrita"
-    }
-]
+    {
+        enunciado: "você ouve música regularmente?",
+        alternativas: [{
+            texto: "sim",
+            afirmação: "você ouve música regularmente? sim, porque me espairece."
+        },
+        {
+            texto: "não",
+            afirmação: "você ouve música regularmente? não, porque só me irrita."
+        }
+        ]
     }
 ]
 let posicao = 0;
@@ -46,31 +48,32 @@ let perguntaAtual;
 let respostas = "";
 
 
-function mostraPergunta(){
-    if (posicao>=perguntas.length){
+function mostraPergunta() {
+    if (posicao >= perguntas.length) {
         mostraResultado();
         return;
     }
     perguntaAtual = perguntas[posicao];
     caixaPergunta.textContent = perguntaAtual.enunciado;
-    caixaAlternativa.textContent = " "; 
+    caixaAlternativa.textContent = " ";
     mostraAlternativas();
 }
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", ()=> respostasSelecionadas(alternativa));
-        caixaAlternativa.appendChild(botaoAlternativas); 
+        botaoAlternativas.addEventListener("click", () => respostasSelecionadas(alternativa));
+        caixaAlternativa.appendChild(botaoAlternativas);
     }
 }
-function respostasSelecionadas(opcaoselecionada){
+function respostasSelecionadas(opcaoselecionada) {
     const afirmacoes = opcaoselecionada.afirmação;
-    respostas += afirmacoes + " "; 
+    
+    respostas += afirmacoes + ' ';
     posicao++;
     mostraPergunta();
 }
-function mostraResultado(){
+function mostraResultado() {
     caixaPergunta.textContent = "confira aqui suas respostas";
     textoResultado.textContent = respostas;
     caixaAlternativa.textContent = "";
